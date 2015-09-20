029c2f7fc5fe1add9fdb71af17e89dd8c18988c1
Martin Sojka <sojka.martin@gmail.com>
2015-09-20 20:37:53 +0200

    Add Results component with tests.

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Router, {Route, DefaultRoute} from 'react-router';
 import App from './components/App'
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const routes = <Route handler={App}>
+  <Route path="/results" handler={Results} />
   <DefaultRoute handler={Voting} />
 </Route>;
 
