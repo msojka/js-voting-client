0619d9ad262df23bb87d47efcc2b06b66e00a6d6
Martin Sojka <sojka.martin@gmail.com>
2015-09-20 21:36:16 +0200

    Init redux store in index.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Router, {Route, DefaultRoute} from 'react-router';
-import App from './components/App'
+import {createStore} from 'redux';
+import reducer from './reducer'
+import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
 
 const routes = <Route handler={App}>
   <Route path="/results" handler={Results} />
